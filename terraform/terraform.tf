c0faae551c359028ee97e45639233c7e70b903f4
Stanislas Lange <git@slange.me>
2022-06-29 13:44:23 +0000

    Add digital ocean provider + update versions

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,18 +2,26 @@ terraform {
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "2.0.0"
+      version = "2.2.2"
+    }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.21.0"
     }
   }
+  required_version = ">= 0.13"
 }
 
+provider "digitalocean" {
+  # Configuration options
+}
 provider "scaleway" {
   zone = "fr-par-1"
 }
 
 terraform {
   backend "s3" {
-    bucket                      = "tereus-tf"
+    bucket                      = "tereus-terraform-state"
     key                         = "state.tfstate"
     region                      = "fr-par"
     endpoint                    = "https://s3.fr-par.scw.cloud"
